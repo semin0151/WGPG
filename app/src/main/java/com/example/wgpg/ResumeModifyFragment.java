74e248b21cae259cfd06770c17dd8706d8b4cd19
semin <semin015123@gmail.com>
2021-02-26 18:22:28 +0900

    Resume/ResumeModify UI & need to set ScrollView

--- app/src/main/java/com/example/wgpg/ResumeModifyFragment.java
+++ app/src/main/java/com/example/wgpg/ResumeModifyFragment.java
@@ -12,26 +12,26 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class ResumeFragment extends Fragment {
+public class ResumeModifyFragment extends Fragment {
     private View view;
-    private Button btn_modify;
+    private Button btn_save;
     private Fragment fragment;
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
 
-    public ResumeFragment(){
+    public ResumeModifyFragment(){
 
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_resume,container, false);
-        btn_modify = view.findViewById(R.id.btn_modify);
-        btn_modify.setOnClickListener(new View.OnClickListener() {
+        view = inflater.inflate(R.layout.fragment_resume_modify,container, false);
+        btn_save = view.findViewById(R.id.btn_save);
+        btn_save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fragment = new ResumeModifyFragment();
+                fragment = new ResumeFragment();
                 fragmentManager = getActivity().getSupportFragmentManager();
                 fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.fl,fragment);
